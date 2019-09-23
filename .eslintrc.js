8e0a6436d321f476e623c272835bf3b1f035f8dd
littleindianboy <saagar@uw.edu>
2019-09-23 14:18:32 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint',
+	],
+	'parser': '@typescript-eslint/parser',
+	"extends": ["plugin:@typescript-eslint/recommended"],
+	'rules': {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'indent': [
+			'error',
+			2
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
